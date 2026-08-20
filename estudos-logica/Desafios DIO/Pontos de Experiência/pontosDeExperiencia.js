332a9170b3a37201a0aa55fa96de480b752a27ea
gustavodeoliveiradev <gustavodeoliveira.dev@gmail.com>
2026-08-20 18:24:20 -0300

    feat(desafio): concluindo desafio Combinando Nomes Pokemons

--- estudos-logica/Desafios DIO/Pontos de Experiência/pontosDeExperiencia.js
+++ estudos-logica/Desafios DIO/Pontos de Experiência/pontosDeExperiencia.js
@@ -1,4 +1,4 @@
-/// Desafios JavaScript na DIO têm funções "gets" e "print" acessíveis globalmente:
+// Desafios JavaScript na DIO têm funções "gets" e "print" acessíveis globalmente:
 // - "gets" : lê UMA linha com dado(s) de entrada (inputs) do usuário;
 // - "print": imprime um texto de saída (output), pulando linha.
 
